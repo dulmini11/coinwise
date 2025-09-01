cac52c6accafa809d53828fb850a871b29b1e289
dulmini11 <dulminihw2@gmail.com>
2025-09-01 23:28:39 +0530

    Add json file with data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
-import Image from "next/image";
+// app/expenses/page.tsx
+"use client";
+import { useState } from "react";
+import expensesData from "../data/expenses.json";
 
-export default function Home() {
+export default function ExpensesPage() {
+
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      
+    <div className="p-8 space-y-6">
     </div>
   );
 }
